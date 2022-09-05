9053c7a12d6a932d21393799edd75368a31e9370
suresh dhakal <dhakalsuresh52@outlook.com>
2022-09-05 14:07:27 +0545

    :sparkles: complete Rollgame

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 
+import { useState } from "react";
 import "./index.css";
 
-function App() {
+const App =() => {
+  const [diceNumber, setDiceNumber]=useState(2)
+  const [diceNumber2, setDiceNumber2]=useState(6)
+
+  const refreshDice =() =>{
+    const ran=Math.floor(Math.random()*6) +1
+    setDiceNumber(ran)
+    const ran2=Math.floor(Math.random()*6) +1
+    setDiceNumber2(ran2)
+  }
   return (
     <div>
       <center>
-        <img width={300} height={300} src={require("./assets/1.png")}></img>
+        <img width={300} height={300} src={require(`./assets/${diceNumber}.png`)}></img>
+        <img width={300} height={300} src={require(`./assets/${diceNumber2}.png`)}></img>
       <br />
-      <button className="button">Roll</button>
+
+      <button onClick={()=> refreshDice()} className="button">Roll</button>
       </center>
     </div>
   );
